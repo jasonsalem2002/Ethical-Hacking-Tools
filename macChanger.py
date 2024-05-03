21b6e95a7a9d25a29f290f38dc8c41247ce805cd
jasonsalem2002 <jason.salem19@hotmail.com>
2024-05-03 20:49:33 +0300

    Added full functionality for MAC Changer

--- macChanger.py
+++ macChanger.py
@@ -19,4 +19,26 @@ def displayHeader():
 
             by Jay and DonC\n""",'green')
         
-print(displayHeader())
+displayHeader()
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC address")
+    options = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC address, use --help for more info.")
+    return options
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+    
+if __name__ == '__main__':
+    options = get_arguments()
+    change_mac(options.interface, options.new_mac)
+    print("[+] MAC address changed to " + options.new_mac)
